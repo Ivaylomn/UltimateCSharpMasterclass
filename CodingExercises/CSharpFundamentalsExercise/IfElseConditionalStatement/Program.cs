24cf8857a530fa5a954e6b68daf2f62a62798466
Ivaylo Ninov <ivailomn216@gmail.com>
2023-10-12 17:38:38 +0300

    initial commit

---
+++ CodingExercises/CSharpFundamentalsExercise/IfElseConditionalStatement/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IfElseConditionalStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            string result;
+
+            if (number < 0)
+            {
+                result = "negative";
+            }
+            else if (number == 0)
+            {
+                result = "zero";
+            }
+            else
+            {
+                result = "positive";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
