44136a6d3ae7ff7b4512286d5e7d0b70e1a7bf49
Ivaylo Ninov <ivailomn216@gmail.com>
2023-10-12 17:04:18 +0300

    Add new exercise

---
+++ Section-2-C#-Fundamentals/Coding-Exersises/CodingExercise2BooleanTypeAndOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CodingExercise2BooleanTypeAndOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 12;
+
+            bool isSumLargerOrEqualTo17 = a + b >= 17;
+
+            Console.WriteLine(isSumLargerOrEqualTo17); 
+        }
+    }
+}
