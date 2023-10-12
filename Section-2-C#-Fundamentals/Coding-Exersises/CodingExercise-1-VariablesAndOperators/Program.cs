122a184eb2d03dac565a8fe5670d43a8e42449bf
Ivaylo Ninov <ivailomn216@gmail.com>
2023-10-12 17:00:51 +0300

    Add new exercise

---
+++ Section-2-C#-Fundamentals/Coding-Exersises/CodingExercise-1-VariablesAndOperators/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CodingExercise_1_VariablesAndOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            int c = 3;
+
+            int result = (a + b) / c;
+
+            Console.WriteLine(result);
+        }
+    }
+}
