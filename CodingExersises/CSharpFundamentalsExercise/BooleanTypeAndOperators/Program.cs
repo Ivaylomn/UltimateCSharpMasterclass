c64e1588563396f78d95133077259a1eaab10e96
Ivaylo Ninov <ivailomn216@gmail.com>
2023-10-12 17:37:00 +0300

    New exercises

---
+++ CodingExersises/CSharpFundamentalsExercise/BooleanTypeAndOperators/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BooleanTypeAndOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 12;
+
+            bool isSumLargerOrEqualTo10 = (a + b) >= 17;
+            Console.WriteLine(isSumLargerOrEqualTo10); 
+        }
+    }
+}
---
+++ CodingExersises/CSharpFundamentalsExercise/VariablesAndOperators/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace VariablesAndOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 5;
+            var b = 10;
+            var c = -4;
+
+            int result = (a + b) / c;
+
+            Console.WriteLine(result); 
+
+        }
+    }
+}
