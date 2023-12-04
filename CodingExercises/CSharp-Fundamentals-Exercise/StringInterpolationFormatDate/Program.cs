45a073a43520670dcb62f893b6bcedb6fd9026eb
Ivaylo <ivailomn216@gmail.com>
2023-12-04 13:53:51 +0200

    Add new coding exercise

---
+++ CodingExercises/CSharp-Fundamentals-Exercise/StringInterpolationFormatDate/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StringInterpolationFormatDate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FormatDate(2023, 12, 04));
+        }
+
+        public static string FormatDate(int year, int month, int day)
+        {
+            return $"{year}/{month}/{day}";
+        }
+    }
+}
