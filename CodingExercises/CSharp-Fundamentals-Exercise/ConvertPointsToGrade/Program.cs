d72070e3f024acd78cfbaa4d9836a4ce34b904ed
Ivaylo <ivailomn216@gmail.com>
2023-12-04 14:25:47 +0200

    Refactoring exercise "ConvertPointsToGrade"

--- CodingExercises/CSharp-Fundamentals-Exercise/ConvertPointsToGrade/Program.cs
+++ CodingExercises/CSharp-Fundamentals-Exercise/ConvertPointsToGrade/Program.cs
@@ -11,28 +11,28 @@
             Console.WriteLine(PointsToGrade(11));
         }
 
-        public static string PointsToGrade(int points)
+        public static char PointsToGrade(int points)
         {
             switch (points)
             {
                 case 10:
                 case 9:
-                    return "A";
+                    return 'A';
                 case 8:
                 case 7:
                 case 6:
-                    return "B";
+                    return 'B';
                 case 5:
                 case 4:
                 case 3:
-                    return "C";
+                    return 'C';
                 case 2:
                 case 1:
-                    return "D";
+                    return 'D';
                 case 0:
-                    return "E";
+                    return 'E';
                 default:
-                    return "!";
+                    return '!';
             }
         }
     }
