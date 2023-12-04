d776d89b05394b5caa255152a20ff11d032bd243
Ivaylo <ivailomn216@gmail.com>
2023-12-04 14:04:36 +0200

    Add new exercise

---
+++ CodingExercises/CSharp-Fundamentals-Exercise/ConvertPointsToGrade/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConvertPointsToGrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PointsToGrade(10));
+            Console.WriteLine(PointsToGrade(1));
+            Console.WriteLine(PointsToGrade(4));
+            Console.WriteLine(PointsToGrade(0));
+            Console.WriteLine(PointsToGrade(11));
+        }
+
+        public static string PointsToGrade(int points)
+        {
+            switch (points)
+            {
+                case 10:
+                case 9:
+                    return "A";
+                case 8:
+                case 7:
+                case 6:
+                    return "B";
+                case 5:
+                case 4:
+                case 3:
+                    return "C";
+                case 2:
+                case 1:
+                    return "D";
+                case 0:
+                    return "E";
+                default:
+                    return "!";
+            }
+        }
+    }
+}
