1690e7e24313f0c8bfb0c7fb714fb83ca88e2da1
Ivaylo <ivailomn216@gmail.com>
2023-12-04 14:47:18 +0200

    Add new exercise

---
+++ CodingExercises/CSharp-Fundamentals-Exercise/WhileLoopCalculateSumBetweenTwoNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace WhileLoopCalculateSumBetweenTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            int result = CalculateSumOfNumbersBetween(numberOne, numberTwo);
+
+            Console.WriteLine(result);
+        }
+
+        public static int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+        {
+            var sum = 0;
+            while (firstNumber <= lastNumber)
+            {
+                sum += firstNumber;
+                firstNumber++;
+            }
+
+            return sum;
+        }
+    }
+}
