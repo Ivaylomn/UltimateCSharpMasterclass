a225530de5acc30e2bac9173a6b0806acd1b3527
Ivaylo <ivailomn216@gmail.com>
2023-12-04 16:04:41 +0200

    Add new exercise

---
+++ CodingExercises/CSharp-Fundamentals-Exercise/DoWhileLoopRepeatCharacter/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DoWhileLoopRepeatCharacter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RepeatCharacter('A', 2));
+            Console.WriteLine(RepeatCharacter('A', 0));
+            Console.WriteLine(RepeatCharacter('B', 5));
+            Console.WriteLine(RepeatCharacter('B', 0));
+        }
+
+        public static string RepeatCharacter(char character, int targetLength)
+        {
+            string repeated = string.Empty;
+            do
+            {
+                repeated += character;
+            }
+            while (repeated.Length < targetLength);
+
+            return repeated;
+        }
+    }
+}
---
+++ CodingExercises/CSharp-Fundamentals-Exercise/ForLoopFactorial/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ForLoopFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(5));
+            Console.WriteLine(Factorial(10));
+            Console.WriteLine(Factorial(15));
+        }
+
+        public static int Factorial(int number)
+        {
+            int sumFactorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                sumFactorial *= i;
+            }
+
+            return sumFactorial;
+        }
+    }
+}
