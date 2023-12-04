be2cb772b071c9b86694c254540d68c1c1ce2a9e
Ivaylo <ivailomn216@gmail.com>
2023-12-04 14:22:16 +0200

    Add new exercise

---
+++ CodingExercises/CSharp-Fundamentals-Exercise/SwitchStatementDescribeDay/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SwitchStatementDescribeDay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(DescribeDay(number));
+        }
+
+        public static string DescribeDay(int dayNumber)
+        {
+            switch (dayNumber)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    return "Working day";
+                case 6:
+                case 7:
+                    return "Weekend";
+                default:
+                    return "Invalid day number";
+            }
+        }
+    }
+}
