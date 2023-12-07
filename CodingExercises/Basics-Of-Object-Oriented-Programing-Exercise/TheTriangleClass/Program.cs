f76c6031e6684dcb511da1ba6ac3ccfde6030d83
Ivaylo <ivailomn216@gmail.com>
2023-12-07 18:09:23 +0200

    Add new exercise and change project directory

---
+++ CodingExercises/Basics-Of-Object-Oriented-Programing-Exercise/TheTriangleClass/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TheTriangleClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Triangle(5, 10);
+            Console.WriteLine(obj.CalculateArea());
+            Console.WriteLine(obj.AsString());
+        }
+    }
+}
---
+++ CodingExercises/Basics-Of-Object-Oriented-Programing-Exercise/TheTriangleClass/Triangle.cs
@@ -0,0 +1,24 @@
+﻿namespace TheTriangleClass
+{
+    public class Triangle
+    {
+        private int _base;
+        private int _height;
+
+        public Triangle(int @base, int height)
+        {
+            _base = @base;
+            _height = height;
+        }
+
+        public double CalculateArea()
+        {
+            return _base * _height / 2;
+        }
+
+        public string AsString()
+        {
+            return $"Base is {_base}, height is {_height}";
+        }
+    }
+}
